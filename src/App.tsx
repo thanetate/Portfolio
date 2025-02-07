72cfc83a9d225e06991111d7ed75e1fb95bf54e0
Tate, Thane <thanetate@my.unt.edu>
2025-02-06 19:51:49 -0600

    reload transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Head from "./components/Head/Head";
 import RightSide from "./components/RightSide/RightSide";
-import { useEffect } from "react";
 import Toggle from "./components/Toggle/Toggle";
+import { motion } from "framer-motion";
 
 function App() {
 	const [theme, setTheme] = useState("dark");
@@ -22,11 +22,19 @@ function App() {
 		setTheme((prevTheme: string) => (prevTheme === "dark" ? "light" : "dark"));
 	};
 	return (
+		<motion.div
+			initial={{opacity: 0}}
+			animate={{opacity: 1}}
+			exit={{opacity: 0}}
+			transition={{duration: 1.3}}
+		>
+
 		<div className="parent-container">
 			<Toggle theme={theme} toggleTheme={toggleTheme} />
 			<Head />
 			<RightSide />
 		</div>
+		</motion.div>
 	);
 }
 
